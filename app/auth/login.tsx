c95dc07a42fa31904c7722ecc686cfce8192001f
taeyun <lwe12f675@gmail.com>
2025-05-13 21:06:18 +0900

    feat: React Hook Form 적용 및 컴포넌트 분리

--- app/auth/login.tsx
+++ app/auth/login.tsx
@@ -1,17 +1,34 @@
+import { type FormValues } from "@/app/auth/signup";
+import EmailInput from "@/components/EmailInput";
 import FixedBottomCTA from "@/components/FixedBottomCTA";
-import InputField from "@/components/InputField";
+import PasswordInput from "@/components/PasswordInput";
 import React from "react";
+import { FormProvider, useForm } from "react-hook-form";
 import { StyleSheet, View } from "react-native";
 
 const LoginScreen = () => {
+  const loginForm = useForm<FormValues>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (formValues: FormValues) => {
+    console.log(formValues);
+  };
+
   return (
-    <>
+    <FormProvider {...loginForm}>
       <View style={styles.container}>
-        <InputField label="이메일" placeholder="이메일을 입력해주세요." />
-        <InputField label="비밀번호" placeholder="비밀번호를 입력해주세요." />
+        <EmailInput />
+        <PasswordInput />
       </View>
-      <FixedBottomCTA label="로그인 하기" onPress={() => {}} />
-    </>
+      <FixedBottomCTA
+        label="로그인 하기"
+        onPress={loginForm.handleSubmit(onSubmit)}
+      />
+    </FormProvider>
   );
 };
 
--- app/auth/signup.tsx
+++ app/auth/signup.tsx
@@ -1,59 +1,43 @@
+import EmailInput from "@/components/EmailInput";
 import FixedBottomCTA from "@/components/FixedBottomCTA";
-import InputField from "@/components/InputField";
-import React, { useState } from "react";
+import PasswordConfirmInput from "@/components/PasswordConfirmInput";
+import PasswordInput from "@/components/PasswordInput";
+import React from "react";
+import { FormProvider, useForm } from "react-hook-form";
 import { StyleSheet, View } from "react-native";
 
+export type FormValues = {
+  email: string;
+  password: string;
+  passwordConfirm: string;
+};
+
 const SignupScreen = () => {
-  const [signupValues, setSignupValues] = useState({
-    email: "",
-    password: "",
-    passwordConfirm: "",
+  const signupForm = useForm<FormValues>({
+    defaultValues: {
+      email: "",
+      password: "",
+      passwordConfirm: "",
+    },
   });
 
-  const [error, setError] = useState({
-    email: "",
-    password: "",
-    passwordConfirm: "",
-  });
-
-  const handleChangeInput = (text: string, name: string) => {
-    setSignupValues((prev) => {
-      return { ...prev, [name]: text };
-    });
-  };
-
-  const handleSubmit = () => {
-    if (signupValues.email.length === 0) {
-      setError((prev) => ({ ...prev, email: "이메일을 입력해주세요." }));
-    }
+  const onSubmit = (formValues: FormValues) => {
+    console.log(formValues);
   };
 
   return (
-    <>
+    <FormProvider {...signupForm}>
       <View style={styles.container}>
-        <InputField
-          label="이메일"
-          placeholder="이메일을 입력해주세요."
-          value={signupValues.email}
-          onChangeText={(text) => handleChangeInput(text, "email")}
-          error={error.email}
-        />
-        <InputField
-          label="비밀번호"
-          placeholder="비밀번호를 입력해주세요."
-          value={signupValues.password}
-          onChangeText={(text) => handleChangeInput(text, "password")}
-        />
-        <InputField
-          label="비밀번호 확인"
-          placeholder="비밀번호를 입력해주세요."
-          value={signupValues.passwordConfirm}
-          onChangeText={(text) => handleChangeInput(text, "passwordConfirm")}
-        />
+        <EmailInput />
+        <PasswordInput />
+        <PasswordConfirmInput />
       </View>
       {/* inset.bottom은 하단 영역을 계산해서 넣어줌. 안드로이드는 inset.bottom이 0 이기때문에 0이면 12px 넣어줌 */}
-      <FixedBottomCTA label="회원가입 하기" onPress={handleSubmit} />
-    </>
+      <FixedBottomCTA
+        label="회원가입 하기"
+        onPress={signupForm.handleSubmit(onSubmit)}
+      />
+    </FormProvider>
   );
 };
 
