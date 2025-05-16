c6ce3e00dfe5356680995ae1431eae9d15f92ab4
taeyun <lwe12f675@gmail.com>
2025-05-16 11:48:44 +0900

    feat: dayjs로 날짜 다루기

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,6 +1,12 @@
 import { colors } from "@/constants";
+import dayjs from "dayjs";
+import "dayjs/locale/ko";
+import relativeTime from "dayjs/plugin/relativeTime";
 import React, { ReactNode } from "react";
 import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+
+dayjs.extend(relativeTime);
+dayjs.locale("ko");
 
 interface ProfileProps {
   onPress: () => void;
@@ -30,7 +36,7 @@ function Profile({
         />
         <View style={{ gap: 4 }}>
           <Text style={styles.nickname}>{nickname}</Text>
-          <Text style={styles.createdAt}>{createdAt}</Text>
+          <Text style={styles.createdAt}>{dayjs(createdAt).fromNow()}</Text>
         </View>
       </Pressable>
       {option}
