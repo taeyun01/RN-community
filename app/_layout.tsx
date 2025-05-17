24da4be85beea37aac3fa7cbe37688a524458509
taeyun <lwe12f675@gmail.com>
2025-05-18 05:36:04 +0900

    feat: React Query DevTools 연동

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,6 @@
 import queryClient from "@/api/queryClient";
 import useAuth from "@/hooks/queries/useAuth";
+import { useReactQueryDevTools } from "@dev-plugins/react-query";
 import { ActionSheetProvider } from "@expo/react-native-action-sheet";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { useFonts } from "expo-font";
@@ -9,6 +10,8 @@ import "react-native-reanimated";
 import Toast from "react-native-toast-message";
 
 export default function RootLayout() {
+  useReactQueryDevTools(queryClient);
+
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
