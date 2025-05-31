d74c1ab1de2107957d9b21bbfc889dfd3ffb98bf
taeyun <lwe12f675@gmail.com>
2025-05-31 22:05:19 +0900

    feat: 아바타 변경 페이지 구현(2) - 코디 저장 (진행중)

--- app/profile/avatar.tsx
+++ app/profile/avatar.tsx
@@ -1,10 +1,13 @@
 import AvatarItem from "@/components/AvatarItem";
 import FixedBottomCTA from "@/components/FixedBottomCTA";
 import Tab from "@/components/Tab";
+import { colors } from "@/constants";
+import useAuth from "@/hooks/queries/useAuth";
 import useGetAvatarItems from "@/hooks/queries/useGetAvatarItems";
 import React, { useRef, useState } from "react";
 import { FlatList, StyleSheet, View } from "react-native";
 import PagerView from "react-native-pager-view";
+import SvgUri from "react-native-svg";
 
 const avatarTabs = ["모자", "얼굴", "상의", "하의", "손", "피부"] as const;
 
@@ -13,16 +16,63 @@ export default function AvatarScreen() {
   const [currentTab, setCurrentTab] = useState(0);
   const { hats, faces, tops, bottoms, hands, skins } = useGetAvatarItems();
   const avatarItems = [hats, faces, tops, bottoms, hands, skins];
+  const { auth } = useAuth();
+  const [avatarItem, setAvatarItem] = useState({
+    hatId: auth?.hatId ?? "",
+    faceId: auth?.faceId ?? "",
+    topId: auth?.topId ?? "",
+    bottomId: auth?.bottomId ?? "",
+    handId: auth?.handId ?? "",
+    skinId: auth?.skinId ?? "",
+  });
   // console.log("avatarItems: ", avatarItems);
+
+  const handlePressItem = (name: string, item: string) => {
+    setAvatarItem((prev) => ({ ...prev, [name]: getImageId(item) }));
+  };
 
   const handlePressTab = (index: number) => {
     pagerRef.current?.setPage(index);
+    // hats/01.png
     setCurrentTab(index);
   };
 
   return (
     <>
       <View style={styles.container}>
+        <View style={styles.headerContainer}>
+          <View style={styles.avatarContainer}>
+            {/* 아이템 카테고리와 그 카테고리의 해당하는 id를 넣으면 서버에서 아바타 이미지를 반환하는 함수 */}
+            <SvgUri
+              uri={getAvatarItemUri("hats", avatarItem.hatId)}
+              style={[styles.avatar, { zIndex: 70 }]}
+            />
+            <SvgUri
+              uri={getAvatarItemUri("faces", avatarItem.faceId)}
+              style={[styles.avatar, { zIndex: 60 }]}
+            />
+            <SvgUri
+              uri={getAvatarItemUri("tops", avatarItem.topId)}
+              style={[styles.avatar, { zIndex: 50 }]}
+            />
+            <SvgUri
+              uri={getAvatarItemUri("bottoms", avatarItem.bottomId)}
+              style={[styles.avatar, { zIndex: 40 }]}
+            />
+            <SvgUri
+              uri={getAvatarItemUri("default")}
+              style={[styles.avatar, { zIndex: 30 }]}
+            />
+            <SvgUri
+              uri={getAvatarItemUri("skins", avatarItem.skinId)}
+              style={[styles.avatar, { zIndex: 20 }]}
+            />
+            <SvgUri
+              uri={getAvatarItemUri("hands", avatarItem.handId)}
+              style={[styles.avatar, { zIndex: 10 }]}
+            />
+          </View>
+        </View>
         <View style={styles.tabContainer}>
           {avatarTabs.map((tab, index) => (
             <Tab
@@ -49,7 +99,11 @@ export default function AvatarScreen() {
                 numColumns={3}
                 contentContainerStyle={styles.listContainer}
                 renderItem={({ item }) => (
-                  <AvatarItem uri={item} isSelected={false} />
+                  <AvatarItem
+                    uri={item}
+                    isSelected={false}
+                    onPress={() => handlePressItem(avatarTabs[index], item)}
+                  />
                 )}
               />
             );
@@ -119,6 +173,28 @@ export default function AvatarScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  headerContainer: {
+    alignItems: "center",
+    position: "relative",
+    backgroundColor: colors.ORANGE_200,
+    width: "100%",
+    height: 115,
+    marginBottom: 115,
+  },
+  avatarContainer: {
+    width: 229, // 서버에서도 아바타 크기가 229로 옴
+    height: 229,
+    borderRadius: 229,
+    borderWidth: 1,
+    overflow: "hidden",
+    borderColor: colors.GRAY_200,
+    backgroundColor: colors.WHITE,
+  },
+  avatar: {
+    width: 229,
+    height: 229,
+    position: "absolute",
   },
   listContainer: {
     paddingBottom: 120,
